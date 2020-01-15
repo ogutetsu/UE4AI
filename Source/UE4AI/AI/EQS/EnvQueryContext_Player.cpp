45fb02328b9704f51fde96d5058e9513071aa4a9
og <ogugu@test.com>
2020-01-15 15:36:06 +0900

    [add] EnvQueryContextの派生クラスを追加

---
+++ Source/UE4AI/AI/EQS/EnvQueryContext_Player.cpp
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnvQueryContext_Player.h"
+#include "EnvironmentQuery/EnvQueryTypes.h"
+#include "EnvironmentQuery/Items/EnvQueryItemType_Actor.h"
+#include "Kismet/GameplayStatics.h"
+#include "Engine/World.h"
+
+
+void UEnvQueryContext_Player::ProvideContext(FEnvQueryInstance& QueryInstance, FEnvQueryContextData& ContextData) const 
+{
+	if (GetWorld())
+	{
+		if (GetWorld()->GetFirstPlayerController())
+		{
+			if (GetWorld()->GetFirstPlayerController()->GetPawn())
+			{
+				UEnvQueryItemType_Actor::SetContextHelper(ContextData, GetWorld()->GetFirstPlayerController()->GetPawn());
+			}
+		}
+	}
+
+}
---
+++ Source/UE4AI/AI/EQS/EnvQueryContext_Player.h
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnvironmentQuery/EnvQueryContext.h"
+#include "EnvQueryContext_Player.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4AI_API UEnvQueryContext_Player : public UEnvQueryContext
+{
+	GENERATED_BODY()
+	
+	virtual void ProvideContext(FEnvQueryInstance& QueryInstance, FEnvQueryContextData& ContextData) const override;
+};
