6ebddd909b6d6b7b400d35f1f3f458d58fc612e9
og <ogugu@test.com>
2020-01-15 10:41:43 +0900

    [add] UE4プロジェクト

---
+++ AISample/Intermediate/Build/Win64/AISampleEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -0,0 +1,1 @@
+#include "D:/GitHub/UE4AI/UE4AI/AISample/Intermediate/Build/Win64/AISampleEditor/Development/Engine/SharedPCH.Engine.h"
---
+++ AISample/Intermediate/Build/Win64/UE4Editor/Inc/AISample/AISampleGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef AISAMPLE_AISampleGameModeBase_generated_h
+#error "AISampleGameModeBase.generated.h already included, missing '#pragma once' in AISampleGameModeBase.h"
+#endif
+#define AISAMPLE_AISampleGameModeBase_generated_h
+
+#define AISample_Source_AISample_AISampleGameModeBase_h_15_SPARSE_DATA
+#define AISample_Source_AISample_AISampleGameModeBase_h_15_RPC_WRAPPERS
+#define AISample_Source_AISample_AISampleGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define AISample_Source_AISample_AISampleGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAAISampleGameModeBase(); \
+	friend struct Z_Construct_UClass_AAISampleGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AAISampleGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/AISample"), NO_API) \
+	DECLARE_SERIALIZER(AAISampleGameModeBase)
+
+
+#define AISample_Source_AISample_AISampleGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAAISampleGameModeBase(); \
+	friend struct Z_Construct_UClass_AAISampleGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AAISampleGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/AISample"), NO_API) \
+	DECLARE_SERIALIZER(AAISampleGameModeBase)
+
+
+#define AISample_Source_AISample_AISampleGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AAISampleGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AAISampleGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AAISampleGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AAISampleGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AAISampleGameModeBase(AAISampleGameModeBase&&); \
+	NO_API AAISampleGameModeBase(const AAISampleGameModeBase&); \
+public:
+
+
+#define AISample_Source_AISample_AISampleGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AAISampleGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AAISampleGameModeBase(AAISampleGameModeBase&&); \
+	NO_API AAISampleGameModeBase(const AAISampleGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AAISampleGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AAISampleGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AAISampleGameModeBase)
+
+
+#define AISample_Source_AISample_AISampleGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define AISample_Source_AISample_AISampleGameModeBase_h_12_PROLOG
+#define AISample_Source_AISample_AISampleGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	AISample_Source_AISample_AISampleGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	AISample_Source_AISample_AISampleGameModeBase_h_15_SPARSE_DATA \
+	AISample_Source_AISample_AISampleGameModeBase_h_15_RPC_WRAPPERS \
+	AISample_Source_AISample_AISampleGameModeBase_h_15_INCLASS \
+	AISample_Source_AISample_AISampleGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define AISample_Source_AISample_AISampleGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	AISample_Source_AISample_AISampleGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	AISample_Source_AISample_AISampleGameModeBase_h_15_SPARSE_DATA \
+	AISample_Source_AISample_AISampleGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	AISample_Source_AISample_AISampleGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	AISample_Source_AISample_AISampleGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> AISAMPLE_API UClass* StaticClass<class AAISampleGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID AISample_Source_AISample_AISampleGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
