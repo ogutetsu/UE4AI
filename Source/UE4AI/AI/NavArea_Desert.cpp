5bb2946342e64e208d78840dbfd929659300f6fd
og <ogugu@test.com>
2020-01-15 12:22:29 +0900

    [add] NavAreaの派生クラスを追加
    BPのJungleを追加
    CPPのDesertを追加

---
+++ Source/UE4AI/AI/NavArea_Desert.cpp
@@ -0,0 +1,11 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NavArea_Desert.h"
+
+UNavArea_Desert::UNavArea_Desert()
+{
+	DefaultCost = 1.5f;
+	FixedAreaEnteringCost = 3.f;
+	DrawColor = FColor::Yellow;
+}
---
+++ Source/UE4AI/AI/NavArea_Desert.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NavAreas/NavArea.h"
+#include "NavArea_Desert.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4AI_API UNavArea_Desert : public UNavArea
+{
+	GENERATED_BODY()
+	
+public:
+	UNavArea_Desert();
+
+};
