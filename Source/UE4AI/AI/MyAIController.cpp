dc43b98bbf6556c5b0e70753a04aed6a387c1819
og <ogugu@test.com>
2020-01-15 11:36:52 +0900

    [add] CPPのAIControllerを追加

---
+++ Source/UE4AI/AI/MyAIController.cpp
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyAIController.h"
+
+
+void AMyAIController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+	AUE4AICharacter* AIChar = Cast<AUE4AICharacter>(InPawn);
+	if (AIChar != nullptr)
+	{
+		UBehaviorTree* BehaviorTree = AIChar->BehaviorTree;
+		if (BehaviorTree != nullptr)
+		{
+			RunBehaviorTree(BehaviorTree);
+		}
+	}
+}
+
---
+++ Source/UE4AI/AI/MyAIController.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "UE4AI/UE4AICharacter.h"
+#include "MyAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4AI_API AMyAIController : public AAIController
+{
+	GENERATED_BODY()
+
+protected:
+
+	void OnPossess(APawn* InPawn) override;
+
+	
+};
