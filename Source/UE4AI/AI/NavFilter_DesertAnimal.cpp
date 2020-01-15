6ad9756569c45ddb73955995c68cf436bcb4943a
og <ogugu@test.com>
2020-01-15 14:46:53 +0900

    [add] ナビゲーションフィルタの追加
    BPとCPPの両方を追加

---
+++ Source/UE4AI/AI/NavFilter_DesertAnimal.cpp
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NavFilter_DesertAnimal.h"
+#include "NavArea_Desert.h"
+
+UNavFilter_DesertAnimal::UNavFilter_DesertAnimal()
+{
+	FNavigationFilterArea Desert = FNavigationFilterArea();
+
+	Desert.AreaClass = UNavArea_Desert::StaticClass();
+
+	Desert.bOverrideEnteringCost = true;
+	Desert.EnteringCostOverride = 0.f;
+
+	Desert.bOverrideTravelCost = true;
+	Desert.TravelCostOverride = 0.8f;
+
+	Areas.Add(Desert);
+
+}
---
+++ Source/UE4AI/AI/NavFilter_DesertAnimal.h
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NavFilters/NavigationQueryFilter.h"
+#include "NavFilter_DesertAnimal.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4AI_API UNavFilter_DesertAnimal : public UNavigationQueryFilter
+{
+	GENERATED_BODY()
+	
+	UNavFilter_DesertAnimal();
+};
