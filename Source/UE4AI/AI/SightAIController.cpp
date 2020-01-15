5a8b9bcb95917a13213dc80f6189672cf2f40744
og <ogugu@test.com>
2020-01-15 16:31:07 +0900

    [add] SightAIControllerクラスの追加

---
+++ Source/UE4AI/AI/SightAIController.cpp
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SightAIController.h"
+#include "Engine/Engine.h"
+
+ASightAIController::ASightAIController()
+{
+	PerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("PerceptionComponent"));
+	SightConfig = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("Sight Config"));
+
+	SightConfig->SightRadius = 1000;
+	SightConfig->LoseSightRadius = 1500;
+	SightConfig->PeripheralVisionAngleDegrees = 130.f;
+
+	SightConfig->DetectionByAffiliation.bDetectEnemies = true;
+	SightConfig->DetectionByAffiliation.bDetectNeutrals = true;
+	SightConfig->DetectionByAffiliation.bDetectFriendlies = true;
+
+	PerceptionComponent->ConfigureSense(*SightConfig);
+	PerceptionComponent->SetDominantSense(SightConfig->GetSenseImplementation());
+
+	PerceptionComponent->OnTargetPerceptionUpdated.AddDynamic(this, &ASightAIController::OnTargetPerceptionUpdate);
+
+}
+
+void ASightAIController::OnTargetPerceptionUpdate(AActor * Actor, FAIStimulus Stimulus)
+{
+	TArray<AActor*> PerceivedActors;
+	PerceptionComponent->GetCurrentlyPerceivedActors(TSubclassOf<UAISense_Sight>(), PerceivedActors);
+
+	bool isEntered = PerceivedActors.Contains(Actor);
+	int NumberObjectSeen = PerceivedActors.Num();
+
+	FString text = FString(Actor->GetName() + " has just " + (isEntered ? "Entered" : "Left") + 
+		" the field of view. Now " + FString::FromInt(NumberObjectSeen) + " objects are visible.");
+
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Turquoise, text);
+	}
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *text);
+
+}
---
+++ Source/UE4AI/AI/SightAIController.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISense_Sight.h"
+#include "Perception/AISenseConfig_Sight.h"
+#include "SightAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4AI_API ASightAIController : public AAIController
+{
+	GENERATED_BODY()
+
+
+public:
+
+
+	ASightAIController();
+
+	UFUNCTION()
+	void OnTargetPerceptionUpdate(AActor* Actor, FAIStimulus Stimulus);
+
+
+	
+	UPROPERTY()
+	UAISenseConfig_Sight* SightConfig;
+
+
+
+	
+};
