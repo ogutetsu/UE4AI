a164be7e5df4644218d89bfee5a08155263578f0
og <ogugu@test.com>
2020-01-16 10:57:46 +0900

    [add] ChasingAICotrollerクラスを追加

---
+++ Source/UE4AI/AI/ChasingAIController.cpp
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ChasingAIController.h"
+
+
+AChasingAIController::AChasingAIController()
+{
+	PerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("SightPerceptionComponent"));
+
+	SightConfig = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("Sight Config"));
+	SightConfig->DetectionByAffiliation.bDetectEnemies = true;
+	SightConfig->DetectionByAffiliation.bDetectNeutrals = true;
+	SightConfig->DetectionByAffiliation.bDetectFriendlies = true;
+
+	PerceptionComponent->ConfigureSense(*SightConfig);
+	PerceptionComponent->SetDominantSense(SightConfig->GetSenseImplementation());
+
+	PerceptionComponent->OnTargetPerceptionUpdated.AddDynamic(this, &AChasingAIController::OnTargetPerceptionUpdate);
+
+}
+
+void AChasingAIController::OnTargetPerceptionUpdate(AActor * Actor, FAIStimulus Stimulus)
+{
+	if (Actor)
+	{
+		LastKnownPlayerPosition = Stimulus.StimulusLocation;
+		bCanSeePlayer = Stimulus.WasSuccessfullySensed();
+	}
+}
---
+++ Source/UE4AI/AI/ChasingAIController.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISense_Sight.h"
+#include "Perception/AISenseConfig_Sight.h"
+#include "ChasingAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4AI_API AChasingAIController : public AAIController
+{
+	GENERATED_BODY()
+
+	AChasingAIController();
+
+	UAISenseConfig_Sight* SightConfig;
+
+public:
+
+	FVector LastKnownPlayerPosition;
+	bool bCanSeePlayer;
+
+
+protected:
+	UFUNCTION()
+	void OnTargetPerceptionUpdate(AActor* Actor, FAIStimulus Stimulus);
+
+};
