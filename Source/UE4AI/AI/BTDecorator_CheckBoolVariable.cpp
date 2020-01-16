cf74448100d909329b496ed21ce2f81845c4b6eb
og <ogugu@test.com>
2020-01-16 10:17:35 +0900

    [add] Cpp用のDecoratorクラスを追加

---
+++ Source/UE4AI/AI/BTDecorator_CheckBoolVariable.cpp
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTDecorator_CheckBoolVariable.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+
+bool UBTDecorator_CheckBoolVariable::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+	const UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	if (BlackboardComp == nullptr)
+	{
+		return false;
+	}
+	return BlackboardComp->GetValueAsBool(BoolVariableToCheck.SelectedKeyName);
+}
+
+
+
+FString UBTDecorator_CheckBoolVariable::GetStaticDescription() const
+{
+	return FString::Printf(TEXT("%s: '%s'"), TEXT("Bool Variable to Check"), BoolVariableToCheck.IsSet() ? *BoolVariableToCheck.SelectedKeyName.ToString():TEXT(""));
+}
+
---
+++ Source/UE4AI/AI/BTDecorator_CheckBoolVariable.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTDecorator.h"
+#include "BTDecorator_CheckBoolVariable.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4AI_API UBTDecorator_CheckBoolVariable : public UBTDecorator
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditAnywhere, Category = Blackboard)
+	FBlackboardKeySelector BoolVariableToCheck;
+
+public:
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+
+	virtual FString GetStaticDescription() const override;
+
+	
+};
