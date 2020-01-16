6bc09794585f5b0af8aa5bde9dc3affc7dc9f5aa
og <ogugu@test.com>
2020-01-16 11:41:02 +0900

    [add] BTServiceクラスを追加

---
+++ Source/UE4AI/AI/BTService_UpdateChasing.cpp
@@ -0,0 +1,70 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTService_UpdateChasing.h"
+#include "ChasingAIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+
+UBTService_UpdateChasing::UBTService_UpdateChasing(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	bNotifyBecomeRelevant = true;
+	bNotifyCeaseRelevant = false;
+}
+
+void UBTService_UpdateChasing::OnBecomeRelevant(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
+{
+
+	UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	if (BlackboardComp == nullptr)
+	{
+		return;
+	}
+
+	if (!PlayerKey.IsSet())
+	{
+		TArray<AActor*> FoundActors;
+		UGameplayStatics::GetAllActorsOfClass(GetWorld(), PlayerClass, FoundActors);
+		if (FoundActors[0])
+		{
+			BlackboardComp->SetValueAsObject(PlayerKey.SelectedKeyName, FoundActors[0]);
+		}
+	}
+
+
+}
+
+void UBTService_UpdateChasing::TickNode(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory, float DeltaSeconds)
+{
+	UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	if (BlackboardComp == nullptr)
+	{
+		return;
+	}
+
+	AAIController* AIController = OwnerComp.GetAIOwner();
+	if (!AIController) 
+	{
+		return;
+	}
+
+	AChasingAIController* ChasingController = Cast< AChasingAIController>(AIController);
+	if (!ChasingController)
+	{
+		return;
+	}
+
+	BlackboardComp->SetValueAsBool(CanSeePlayerKey.SelectedKeyName, ChasingController->bCanSeePlayer);
+
+	if (ChasingController->bCanSeePlayer != bLastCanSeePlayer)
+	{
+		BlackboardComp->SetValueAsVector(LastKnownPositionKey.SelectedKeyName, ChasingController->LastKnownPlayerPosition);
+	}
+
+	bLastCanSeePlayer = ChasingController->bCanSeePlayer;
+
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+}
